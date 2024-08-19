9d7f893e9c54ffdebe87ff7b0fc2ab7f42182bdb
egal <egal@qq.com>
2024-08-19 15:32:13 +0800

    build优化

--- proto-rs-server/build.rs
+++ proto-rs-server/build.rs
@@ -1,6 +1,10 @@
-use std::env;
-
 fn main() {
+    let build_enabled = option_env!("BUILD_PROTO").map(|v| v == "1").unwrap_or(false);
+
+    if !build_enabled {
+        println!("=== Skipped compiling protos ===");
+        return;
+    }
     //     prost_build::Config::new()
     //         .default_package_filename("animals")
     //         .out_dir("src/pb")
--- proto-rs-server/src/pb/mod.rs
+++ proto-rs-server/src/pb/mod.rs
@@ -1,3 +1,2 @@
 pub mod student;
 
-use student::*;
